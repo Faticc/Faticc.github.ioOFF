9e3cf0fcef2970120ad92124e9cfc2fad8f5961d
Faticc <114472282+Faticc@users.noreply.github.com>
2023-02-12 13:46:05 +0000

    modified:   js/test.js

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 let people = [
-    { name: "Александр", vl: 0 },
-    { name: "Петрович", vl: 1 },
-    { name: "Вова", vl: 1 }
+    { name: "Александр", vl: "0" },
+    { name: "Петрович", vl: "1" },
+    { name: "Вова", vl: "1" }
   ];
   function test() {
       for (let i = 0; i < people.length; i++) {
