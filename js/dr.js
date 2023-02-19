51af775d569001e67da0347d07b8d49dc30b654f
komeristyj <komeristyj07@gmail.com>
2023-02-19 23:28:28 +0300

    modified:   js/dr.js

--- js/dr.js
+++ js/dr.js
@@ -3,7 +3,7 @@ let people = [
     { name: "Абрамову Марию", birthday: new Date("01/27/2009"), gender: "female" },
     { name: "Амирова Эмиля", birthday: new Date("02/11/2009"), gender: "male" },
     { name: "Бабаева Бориса", birthday: new Date("09/29/2008"), gender: "male" },
-    { name: "Бадалян Шушану", birthday: new Date("05/21/2008"), gender: "female" },
+    { name: "Бадалян Шушанну", birthday: new Date("05/21/2008"), gender: "female" },
     { name: "Гаврилова Виктора", birthday: new Date("MM/dd/2006"), gender: "male" },
     { name: "Голик Алёну", birthday: new Date("03/08/2008"), gender: "female" },
     { name: "Голикова Тимура", birthday: new Date("05/dd/2008"), gender: "male" },
@@ -20,7 +20,7 @@ let people = [
     { name: "Меретину Милану", birthday: new Date("12/27/2007"), gender: "female" },
     { name: "Немогучеву Юлию", birthday: new Date("05/26/2009"), gender: "female" },
     { name: "Пасова Никиту", birthday: new Date("09/26/2008"), gender: "male" },
-    { name: "Сафаряна Максима", birthday: new Date("09/22/2008"), gender: "male" },
+    { name: "Сафарян Максима", birthday: new Date("09/22/2008"), gender: "male" },
     { name: "Соколова Данила", birthday: new Date("09/24/2008"), gender: "male" },
     { name: "Чехизе Малак", birthday: new Date("01/30/2009"), gender: "female" },
     { name: "Шарлай Алёну", birthday: new Date("09/27/2008"), gender: "female" }
@@ -31,7 +31,9 @@ let people = [
     let today = new Date();
     for (let i = 0; i < people.length; i++) {
       if (today.getMonth() === people[i].birthday.getMonth() && today.getDate() === people[i].birthday.getDate()) {
-          alert(`Поздравим с днем рождения! Желаю вам всего наилучшего в ваш особенный день: ${people[i].name}`);
+          alert(`Поздравляем ученика 8В класса ${people[i].name} с днём рождения Надеемся, он будет здоровым!
+                 И проведёт этот день весело и съест много вкусностей.
+                 Пожалуйста, продолжайте быть счастливыми!`);
       }
     }
   }
